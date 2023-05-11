b9f038b5b49a096239cefe5fd3249edec38773d1
augustinejoseph <augustine.augustine4@gmail.com>
2023-05-11 20:49:50 +0530

    created sorting for DSA

---
+++ bubblesort.py
@@ -0,0 +1,11 @@
+# Basic bubble sort
+def bubbleSort(arr):
+    for i in range(0, len(arr)-1):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    print("Basic bubble sort",arr)
+
+
+arr = [5,2,66,34,1,444,87,0,34,-1,90,-34]
+bubbleSort(arr)
